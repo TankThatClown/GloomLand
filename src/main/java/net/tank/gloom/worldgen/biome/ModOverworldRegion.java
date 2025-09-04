549ac680059f0f8fe04077b22aba2522f0121e9a
Hang Jiang <bluepastrock@gmail.com>
2025-09-03 20:21:02 -0400

    Finalize the biome

--- src/main/java/net/tank/gloom/worldgen/biome/ModOverworldRegion.java
+++ src/main/java/net/tank/gloom/worldgen/biome/ModOverworldRegion.java
@@ -7,7 +7,11 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.Climate;
+import terrablender.api.ParameterUtils.*;
 import terrablender.api.Region;
+import terrablender.api.VanillaParameterOverlayBuilder;
+
+import net.minecraft.data.worldgen.*;
 import terrablender.api.RegionType;
 
 import java.util.function.Consumer;
@@ -19,28 +23,25 @@ public class ModOverworldRegion extends Region {
 
     @Override
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper) {
-        final Climate.Parameter temperature = Climate.Parameter.span(-0.45F, -0.15F);      // temperature
-        final Climate.Parameter humidity = Climate.Parameter.span(-1.0F, -0.35F);          // humidity
-        final Climate.Parameter continentalness = Climate.Parameter.span(0.2F,0.6F);  // continentalness
-        final Climate.Parameter erosion = Climate.Parameter.span(-0.78F, -0.375F);           // erosion
-        final Climate.Parameter weirdness = Climate.Parameter.span(-0.2F, 0.2F);       // weirdness
-        final Climate.Parameter depth = Climate.Parameter.point(0.0F);             // depth
-        Climate.ParameterPoint gloomPoint = new Climate.ParameterPoint(
-                temperature,
-                humidity,
-                continentalness,
-                erosion,
-                depth,
-                weirdness,
-                0L
-        );
-       // this.addBiome(mapper,gloomPoint, ModBiomes.GLOOM_PLAINS);
-        this.addBiomeSimilar(mapper, Biomes.PLAINS, ModBiomes.GLOOM_PLAINS);
+        VanillaParameterOverlayBuilder builder = new VanillaParameterOverlayBuilder();
+
+        new ParameterPointListBuilder()
+                .temperature(Temperature.span(Temperature.NEUTRAL, Temperature.WARM))
+                .humidity(Humidity.span(Humidity.ARID, Humidity.DRY))
+                .continentalness(Continentalness.INLAND)
+                .erosion(Erosion.EROSION_0, Erosion.EROSION_1)
+                .depth(Depth.FULL_RANGE)
+                .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING)
+                .build().forEach(point -> builder.add(point, ModBiomes.GLOOM_PLAINS));
+
+        // Add our points to the mapper
+        builder.build().forEach(mapper);
+        /*this.addBiomeSimilar(mapper, Biomes.PLAINS, ModBiomes.GLOOM_PLAINS);
         this.addBiomeSimilar(mapper,Biomes.SUNFLOWER_PLAINS,ModBiomes.GLOOM_PLAINS);
         this.addBiomeSimilar(mapper,Biomes.MEADOW,ModBiomes.GLOOM_PLAINS);
         this.addModifiedVanillaOverworldBiomes(mapper, modifiedVanillaOverworldBuilder -> {
 
-        });
+        });*/
 
     }
 }
